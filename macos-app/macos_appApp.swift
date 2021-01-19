984ea6e422bebb7332cef8bd5f7bd0423663a7fb
Kirill Korobejnikov <6500867@mail.ru>
2021-01-19 13:51:53 +0300

    Initial Commit

---
+++ macos-app/macos_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  macos_appApp.swift
+//  macos-app
+//
+//  Created by Bitard on 1/19/21.
+//
+
+import SwiftUI
+
+@main
+struct macos_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
